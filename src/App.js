2b776413ad1d4946c5453252398833eeafdfed2c
unknown <arunm291003@gmail.com>
2022-07-27 23:11:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages';
 import SigninPage from './pages/signin';
